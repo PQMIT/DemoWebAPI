8ce3792a39bfbf2e7818263e0df7e5a8e4e87a25
Phạm Quang Minh <56736411+PQM2112000@users.noreply.github.com>
2021-11-20 23:34:59 +0700

    bai1,2

---
+++ MyWebApi App/MyWebApi App/Models/HangHoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi_App.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public Double DonGia { get; set; }
+    }
+    
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
