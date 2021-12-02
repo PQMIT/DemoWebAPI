39aed58fa01742e384f01571439fe678ea8830da
Phạm Quang Minh <56736411+PQM2112000@users.noreply.github.com>
2021-12-02 16:25:30 +0700

    addDonHangChiTiet_ Fluent api

---
+++ MyWebApi App/MyWebApi App/Data/HangHoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi_App.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+        [Required]
+        [MaxLength()]
+        public String TenHh { get; set; }
+
+        public string MoTa { get; set; }
+        [Range(0, double.MaxValue)]
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+
+        public ICollection<DonHangChiTiet> DonHangChiTiets { get; set; }
+        
+        // hiển thị kết quả rỗng thay vì hiện null nên dùng list hoặc hashset
+        public HangHoa()
+        {
+            DonHangChiTiets = new HashSet<DonHangChiTiet>();
+        }
+    }
+}
